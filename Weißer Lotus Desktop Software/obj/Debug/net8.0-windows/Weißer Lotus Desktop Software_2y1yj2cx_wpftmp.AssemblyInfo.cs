a4b447c1561bff96d67db872e2c097e35d37a32c
JanSchoenberger <janschoenberger@outlook.de>
2024-02-09 14:03:35 +0100

    Upload Zwischenstand für die Sicherung. Arbeit an der Datenbankimplementierung.

--- Weißer Lotus Desktop Software/obj/Debug/net8.0-windows/Weißer Lotus Desktop Software_2y1yj2cx_wpftmp.AssemblyInfo.cs
+++ Weißer Lotus Desktop Software/obj/Debug/net8.0-windows/Weißer Lotus Desktop Software_2y1yj2cx_wpftmp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Weißer Lotus Desktop Software")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f06fc1ab877beb896077617d8d14c43ef5ecae9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Weißer Lotus Desktop Software")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Weißer Lotus Desktop Software")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
